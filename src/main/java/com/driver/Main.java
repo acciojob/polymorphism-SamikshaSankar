74fa124b3f42b8d5363aca97ec82da16dabe413a
Samiksha sandip sankar <samikshasankar2000@gmail.com>
2023-11-21 19:30:03 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,15 +2,19 @@ package com.driver;
 
 public class Main {
     public static class Product{
-        public int product(int x, int y) {
-            return x+y;
+        //Task 3: create a method of following defination public int product(int x, int y) {}
+        public int product(int x, int y){
+            return x*y;
         }
-        public int product(int x, int y, int z) {
-            return x+y+z;
+        //Task 4: create a Overloaded method product of following defination public int product(int x, int y, int z){}
+        public int product(int x, int y, int z){
+            return x*y*z;
         }
-        public double product(double x, double y) {
-            return x+y;
+        //Task 5: create a Overloaded method product of following defination public double product(double x, double y){}
+        public double product(double x, double y){
+            return x*y;
         }
+
     }
     public static void main(String[] args){
         Product p = new Product();
